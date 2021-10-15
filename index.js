7c2e12a410be351f5b3a123178dce8ad4774e591
mellowmorgan <morgan.waites@gmail.com>
2021-10-14 18:01:37 -0700

    DEBUG: got add function working

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-let count = 0;
-console.log(count)
+var count=0
+let countEl = document.getElementById("num2")
+function add(){
+  var countE = document.getElementById("num1").value
+  count+=parseInt(countE)
+  countEl.textContent=count
+}
