b56d42b48b0e5688fd45be153348693181dd6fa1
mellowmorgan <morgan.waites@gmail.com>
2021-10-17 17:43:17 -0700

    Add alert pop-up

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 let countResult=0;
 function add(){
   let countInput = parseInt(document.getElementById("num1").value);
-  countResult+=countInput;
-  document.getElementById("num2").textContent = countResult;
+  if (document.getElementById("num1").value==''){
+    window.alert("Please enter a number.");
+  }
+  else{
+    countResult+=countInput;
+    document.getElementById("num2").textContent = countResult;
+  }
 }
